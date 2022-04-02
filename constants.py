c0248ee05639bc5c02bf078dd4b08bb7cb9de297
Potagashev <alexpotagashev@gmail.com>
2022-04-02 18:02:41 +0700

    modified

--- constants.py
+++ constants.py
@@ -3,13 +3,13 @@ VALID_OUTPUT_FORMATS = ['csv', 'tsv', 'json']
 KEYS = ["first_name", "last_name", "country", "city", "bdate", "sex"]
 TITLES = ["Имя", "Фамилия", "Страна", "Город", "Дата", "Пол"]
 
-URL = "https://api.vk.com/method/friends.get"
+URL = 'https://api.vk.com/method/friends.get'
 V = '5.131'
 FIELDS = 'country, city, bdate, sex'
 ORDER = 'name'
 
-TOKEN = '424e2d433c4e236ce55390fe0c58f22ee214c455d25fcbab909eb3b8562cd5f6d6af151c3ef4e61592fff'
-USER_ID = "222622761"
-OUTPUT_FORMAT = "json"
-OUTPUT_PATH = "reports/report.json"
+TOKEN = ''
+USER_ID = ''
+OUTPUT_FORMAT = 'json'
+OUTPUT_PATH = 'reports/report.json'
 MAX_COUNT = 100
